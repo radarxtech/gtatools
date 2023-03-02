15ec76fb5164704da996ecb20aac95253b56c692
radarxtech <101777504+radarxtech@users.noreply.github.com>
2023-03-02 16:30:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function lookup() {
-  const insurance = document.getElementById("insurance").value;
+  const insurance = document.getElementById("insurance").value.trim();
   fetch("data.csv")
     .then(response => response.text())
     .then(data => {
       const rows = data.split("\n");
+      console.log(rows);
       for (let i = 1; i < rows.length; i++) {
-        const columns = rows[i].split(",");
+        const columns = rows[i].split(",").map(col => col.trim());
+        console.log(columns);
         if (columns[0] === insurance) {
           const result = `Homeowners: ${columns[1]}, Expanded Loan: ${columns[2]}`;
           document.getElementById("result").innerHTML = result;
