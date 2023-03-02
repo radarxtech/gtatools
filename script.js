5cc7ea354b8b73164bd875c0ff1698c46cb3c894
radarxtech <101777504+radarxtech@users.noreply.github.com>
2023-03-02 16:21:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,19 +4,18 @@ function lookup() {
     .then(response => response.text())
     .then(data => {
       const rows = data.split("\n");
-      console.log(rows);
       for (let i = 1; i < rows.length; i++) {
         const columns = rows[i].split(",");
-        console.log(columns);
         if (columns[0] === insurance) {
           const result = `Homeowners: ${columns[1]}, Expanded Loan: ${columns[2]}`;
           document.getElementById("result").innerHTML = result;
           return;
-              }
-            }
-            document.getElementById("result").innerHTML = "No matching data found.";
-          })
-          .catch(error => {
-            console.error(error);
-            document.getElementById("result").innerHTML = "Error: Could not load data.";
-          });
+        }
+      }
+      document.getElementById("result").innerHTML = "No matching data found.";
+    })
+    .catch(error => {
+      console.error(error);
+      document.getElementById("result").innerHTML = "Error: Could not load data.";
+    });
+}
